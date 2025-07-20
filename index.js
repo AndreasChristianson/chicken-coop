cc2a10f14c227927095e314126786c4082bb4ece
Andreas Christianson <achristianson@pessimistic-it.com>
2025-07-20 10:37:20 -0500

    temps too

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import child_process from 'node:child_process';
 import {v4 as uuidv4} from 'uuid';
 import Inert from '@hapi/inert'
 import {Mutex} from 'async-mutex';
+import fs from 'node:fs/promises';
+import convert from "convert";
+
+const thermistors = [
+    {path: '/sys/bus/w1/devices/28-00000037cd2f', name: "one"},
+    {path: '/sys/bus/w1/devices/28-0000005393bc', name: "two"},
+    {path: '/sys/bus/w1/devices/28-0000005668d0', name: "three"},
+    {path: '/sys/bus/w1/devices/28-000000bf3f2e', name: "four"},
+]
 
 const exec = promisify(child_process.exec);
 const mutex = new Mutex();
@@ -12,7 +21,7 @@ async function takePhoto() {
     try {
         const filename = `/app/${uuidv4()}.jpeg`
         console.log(`filename: ${filename}`);
-        const {stdout, stderr} = await mutex.runExclusive(exec(`rpicam-still -o ${filename}`));
+        const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still -o ${filename}`));
         console.log(`camera stdout: ${stdout}`);
         console.log(`camera stderr: ${stderr}`);
         return filename;
@@ -20,6 +29,31 @@ async function takePhoto() {
         console.log(error);
         return null;
     }
+}
+
+async function readTemp(path) {
+    try {
+        console.log(`reading ${path}`);
+        const data = await fs.readFile(`${path}/w1_slave`, {encoding: 'utf8'});
+        console.log(`raw data for ${path}:\n${data}`);
+        if (!/crc=\d\d YES/g.test(data)) {
+            return null
+        }
+        const temp = data.match(/.*t=(\d*)/g)[1] / 1000;
+        return convert(temp, "fahrenheit").to("celsius");
+    } catch (err) {
+        console.error(err);
+    }
+    return null;
+}
+
+async function readTemps() {
+    const promises = thermistors.map(async ({name, path}) => {
+        console.log(`thermistor - ${name}: ${path}`);
+        return {[name]: await readTemp(path)};
+    })
+
+    return await Promise.all(promises)
 }
 
 const PORT = process.env.PORT || 3000
@@ -42,6 +76,14 @@ const init = async () => {
                 return h.file(snapshot)
             }
             return h.response("Snapshot error. See logs.").code(503)
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/temps',
+        handler: async (request, h) => {
+            return h.response(await readTemps());
         }
     });
 
