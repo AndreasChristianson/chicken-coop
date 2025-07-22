554d254ff787878aa5d6dfe4ded3d44b2f255bd8
Andreas Christianson <achristianson@pessimistic-it.com>
2025-07-21 22:12:40 -0500

    map to string

--- src/relays/index.js
+++ src/relays/index.js
@@ -1,10 +1,20 @@
 import relays from "./relays.js";
+import {Gpio} from "onoff";
 
+const statusMap = {
+    [Gpio.HIGH]: "on",
+    [Gpio.LOW]: "off"
+}
+
+const getStatus = async (gpio) => {
+    const status = await gpio.read()
+    return statusMap[status];
+}
 
 const getStatuses = async () => {
     const promises = relays
         .map(async ({gpio, name}) => ({
-            [name]: await gpio.read()
+            [name]: await getStatus(gpio)
         }))
 
     return Promise.all(promises);
