2a37328c12746be89109d1bb61a2297cd337faae
Andreas Christianson <achristianson@pessimistic-it.com>
2025-07-24 20:31:05 -0500

    boom!

--- src/relays/index.js
+++ src/relays/index.js
@@ -1,4 +1,5 @@
 import relays from "./relays.js";
+import Boom from '@hapi/boom';
 import {Gpio} from "onoff";
 
 const statusMap = {
@@ -12,7 +13,11 @@ const reverseStatusMap = {
 }
 
 const getRelay = async (name) => {
-    const {relay, pin} = relays[name];
+    const foundRelay = relays[name];
+    if (!foundRelay) {
+        throw Boom.notFound(name)
+    }
+    const {relay, pin} = foundRelay;
     const status = statusMap[await relay.read()];
     return {
         status,
@@ -21,8 +26,18 @@ const getRelay = async (name) => {
     }
 }
 
-const setRelay = async (name, newStatus) => {
-    const {relay} = relays[name];
+const setRelay = async (name, status) => {
+    const foundRelay = relays[name];
+    if (!foundRelay) {
+        throw Boom.notFound(name)
+    }
+    const {relay} = foundRelay;
+
+    const newStatus = reverseStatusMap[status];
+    if (!newStatus) {
+        throw new Boom.badRequest(status)
+    }
+
     await relay.write(newStatus);
 }
 
@@ -34,32 +49,19 @@ export const relayStatusesHandler = (request, h) => {
 }
 
 export const relayStatusHandler = (request, h) => {
-    if(!relays[request.params.name]){
-        return h.code(404);
-    }
     return h.response(getRelay(request.params.name));
 }
 
 export const relayPatchHandler = async (request, h) => {
-    try {
-        if (!relays[request.params.name]) {
-            return h.code(404);
-        }
-        console.log(request.payload)
-        const body = request.payload;
-        if (!body) {
-            return h.code(400);
-        }
-        const newStatus = reverseStatusMap[body.status];
-        if (!newStatus) {
-            return h.code(400);
-        }
-        await setRelay(request.params.name, newStatus);
-        return h
-            .response(await getRelay(request.params.name))
-            .code(202);
-    } catch (err) {
-        console.error(err);
-        throw err;
+    if (!relays[request.params.name]) {
+        throw Boom.notFound(request.params.name);
     }
+    const body = request.payload;
+    if (!body) {
+        throw Boom.badRequest("no body");
+    }
+    await setRelay(request.params.name, body.status);
+    return h
+        .response(await getRelay(request.params.name))
+        .code(202);
 }
--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -7,23 +7,15 @@ const exec = promisify(child_process.exec);
 const mutex = new Mutex();
 
 const takePhoto = async () => {
-    try {
-        const filename = `/app/${uuidv4()}.jpeg`
-        console.log(`filename: ${filename}`);
-        const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still -o ${filename}`));
-        console.log(`camera stdout: ${stdout}`);
-        console.log(`camera stderr: ${stderr}`);
-        return filename;
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
+    const filename = `/app/${uuidv4()}.jpeg`
+    console.log(`filename: ${filename}`);
+    const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still -o ${filename}`));
+    console.log(`camera stdout: ${stdout}`);
+    console.log(`camera stderr: ${stderr}`);
+    return filename;
 }
 
 export const snapshotHandler = async (request, h) => {
     const snapshot = await takePhoto();
-    if (snapshot) {
-        return h.file(snapshot)
-    }
-    return h.response("Snapshot error. See logs.").code(503)
+    return h.file(snapshot)
 }
