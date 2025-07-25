8fe3951dac52d895325cf775f24ef0b184995f53
Andreas Christianson <achristianson@pessimistic-it.com>
2025-07-24 21:35:47 -0500

    forgot..

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -11,7 +11,7 @@ const takePhoto = async () => {
     const uuid = uuidv4();
     const filename = `/app/${uuid}.jpeg`
     console.log(`filename: ${filename}`);
-    const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still --rotation 90 -o ${filename}`));
+    const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still -o ${filename}`));
     console.log(`camera stdout: ${stdout}`);
     console.log(`camera stderr: ${stderr}`);
     const rotatedFilename = `/app/${uuid}-out.jpeg`
