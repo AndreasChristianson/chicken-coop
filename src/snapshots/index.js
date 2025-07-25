c31c6099f9bb7236b129b8f99bcb5e81c16334c9
Andreas Christianson <achristianson@pessimistic-it.com>
2025-07-24 21:30:26 -0500

    rotate

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -2,17 +2,23 @@ import {promisify} from 'node:util';
 import child_process from 'node:child_process';
 import {v4 as uuidv4} from 'uuid';
 import {Mutex} from 'async-mutex';
+import sharp from 'sharp'
 
 const exec = promisify(child_process.exec);
 const mutex = new Mutex();
 
 const takePhoto = async () => {
-    const filename = `/app/${uuidv4()}.jpeg`
+    const uuid = uuidv4();
+    const filename = `/app/${uuid}.jpeg`
     console.log(`filename: ${filename}`);
     const {stdout, stderr} = await mutex.runExclusive(async () => await exec(`rpicam-still --rotation 90 -o ${filename}`));
     console.log(`camera stdout: ${stdout}`);
     console.log(`camera stderr: ${stderr}`);
-    return filename;
+    const rotatedFilename = `/app/${uuid}-out.jpeg`
+    await sharp(filename)
+        .rotate(90)
+        .toFile(rotatedFilename);
+    return rotatedFilename;
 }
 
 export const snapshotHandler = async (request, h) => {
